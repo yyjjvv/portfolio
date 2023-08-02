f8ee30cf4502217c16d62594c452ba5d55f393f6
yyjjvv <jiwonyoo66@gmail.com>
2023-08-02 16:06:47 +0900

    fix: img 교체

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,11 @@
-// import { useState } from "react";
 import { Link } from "react-router-dom";
-// import Modal from "./Modal";
 import styles from "./List.module.scss";
 
 const List = ({ data }) => {
-    // const [isVisible, setisVisible] = useState(false);
-
-    // const handleModalVisibility = () => {
-    //     if(isVisible){
-    //         document.body.style = `overflow: auto`;
-    //     } else {
-    //         document.body.style= `overflow: hidden`;
-    //     }
-    //     setisVisible(prev => !prev);
-    // }
 
     return (
-        <Link to={`/list/${data.id}`}>
-            <li className={styles["projects__list"]} /*onClick={handleModalVisibility}*/>
+        <li className={styles["projects__list"]} style={{backgroundColor:`${data.color}`}}>
+            <Link to={`/list/${data.id}`}>
                 <div className={styles["projects__list--img"]}>
                     <div>{data.description}</div>
                     <img src={data.img} alt={data.title} />
@@ -26,9 +14,8 @@ const List = ({ data }) => {
                     <h3>{data.title}</h3>
                     <span>{data.type}</span>
                 </div>
-            </li>
-            {/* {isVisible && <Modal data={data} closeModal={handleModalVisibility} />} */}
-        </Link>
+            </Link>
+        </li>
     );
 };
 
--- src/page/Detail.js
+++ src/page/Detail.js
@@ -15,19 +15,20 @@ const Detail = () => {
                 className={styles["content__img"]}
                 style={{
                     backgroundImage: `url('/img/detail_${info.name}.png')`,
+                    backgroundColor: `${info.color}`,
                 }}
             ></div>
             <div className={styles["content__description"]}>
-                <h3 className={styles.title}>{info.title}</h3>
+                <h2 className={styles.title}>{info.title}</h2>
                 <p className={styles.date}>
                     {info.type} / {info.date}
                 </p>
-                <p className={styles["category__title"]}>프로젝트 개요</p>
+                <h3 className={styles["category__title"]}>프로젝트 개요</h3>
                 <div
                     className={styles["category__contents"]}
                     dangerouslySetInnerHTML={{ __html: info.description }}
                 ></div>
-                <p className={styles["category__title"]}>담당 역할 및 기능</p>
+                <h3 className={styles["category__title"]}>담당 역할 및 기능</h3>
                 <ul
                     className={styles["category__lists"]}
                     dangerouslySetInnerHTML={{ __html: info.detailHTML }}
@@ -35,15 +36,26 @@ const Detail = () => {
                     
                 </ul>
                 <div className={styles["category__link"]}>
-                <a href={info.url} className={styles["category__link__list"]}>
-                    <FontAwesomeIcon icon={faLink} />
-                    <span>URL</span>
-                </a>
-                {info.github && (
-                    <a href={info.github} className={styles["category__link__list"]}>
-                        <FontAwesomeIcon icon={faGithub} />GitHub
+                    <a
+                        href={info.url}
+                        target="_blank"
+                        className={styles["category__link__list"]}
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faLink} />
+                        <span>URL</span>
                     </a>
-                )}
+                    {info.github && (
+                        <a
+                            href={info.github}
+                            target="_blank"
+                            className={styles["category__link__list"]}
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={faGithub} />
+                            GitHub
+                        </a>
+                    )}
                 </div>
             </div>
         </main>
